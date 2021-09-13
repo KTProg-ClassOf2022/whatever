4e2479fdca0e3e4cf315e446e06e37f6987836c2
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2021-09-13 12:40:55 -0400

    Finished

--- whatever/Program.cs
+++ whatever/Program.cs
@@ -35,6 +35,20 @@ namespace MotorVehicle1
                 }
             }
 
+            private string _year;
+            public string Year
+            {
+                get
+                {
+                    return _year;
+                }
+                set
+                {
+                    if (value != null)
+                        _year = value;
+                }
+            }
+
             private bool _engineOn;
             public string Engine
             {
@@ -47,11 +61,25 @@ namespace MotorVehicle1
                 }
             }
 
+            private bool _legacy;
+            public string Legacy
+            {
+                get
+                {
+                    if (_legacy)
+                        return "Yes";
+                    else
+                        return "No";
+                }
+            }
+
             public MotorVehicle()
             {
                 _model = "";
                 _make = "";
+                _year = "";
                 _engineOn = false;
+                _legacy = false;
             }
 
             public bool Ignition(string action, string key)
@@ -78,7 +106,7 @@ namespace MotorVehicle1
 
         static void Main(string[] args)
         {
-            const int NUM_VEHICLES = 3;
+            const int NUM_VEHICLES = 4;
             MotorVehicle[] auto = new MotorVehicle[NUM_VEHICLES];
 
             for (int i = 0; i < NUM_VEHICLES; i++)
@@ -88,20 +116,39 @@ namespace MotorVehicle1
 
             auto[0].Make = "Toyota";
             auto[0].Model = "Camry";
+            auto[0].Year = "2012";
 
             auto[1].Make = "Honda";
             auto[1].Model = "Civic";
+            auto[1].Year = "2015";
 
             auto[2].Make = "Mazda";
             auto[2].Model = "3";
+            auto[2].Year = "2017";
+
+            auto[3].Make = "Subaru";
+            auto[3].Model = "Legacy";
+            auto[3].Year = "2017";
 
             for (int i = 0; i < NUM_VEHICLES; i++)
             {
-                Console.WriteLine("{0} {1} {2}", auto[i].Make, auto[i].Model, auto[i].Engine);
+                Console.WriteLine("{0} {1} {2} {3}", auto[i].Year, auto[i].Make, auto[i].Model, auto[i].Engine);
             }
             auto[0].Ignition("On", "1234");
             auto[1].Ignition("On", "0000");
             auto[2].Ignition("On", "4321");
+            auto[1].Ignition("On", "0000");
+
+            Console.WriteLine("--------------");
+            for (int i = 0; i < NUM_VEHICLES; i++)
+            {
+                Console.WriteLine("{0} {1} {2}", auto[i].Make, auto[i].Model, auto[i].Engine);
+            }
+
+            auto[0].Ignition("Off", "1234");
+            auto[1].Ignition("Off", "0000");
+            auto[2].Ignition("Off", "4321");
+            auto[1].Ignition("Off", "0000");
 
             Console.WriteLine("--------------");
             for (int i = 0; i < NUM_VEHICLES; i++)
