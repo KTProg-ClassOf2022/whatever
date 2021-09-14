4bd79bb195bdb0f8d50f40d680b7dc17b4727cd5
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2021-09-14 12:36:54 -0400

    Day 2 part 1

--- whatever/Program.cs
+++ whatever/Program.cs
@@ -40,14 +40,22 @@ namespace MotorVehicle1
             {
                 get
                 {
-                    if (Int32.Parse(_year) >= 1930 && Int32.Parse(_year) <= 2020)
+                    if (_year != "Default")
                     {
-                        return _year;
+                        if (Int32.Parse(_year) >= 1930 && Int32.Parse(_year) <= 2020)
+                        {
+                            return _year;
+                        }
+                        else
+                        {
+                            return "2020";
+                        }
                     }
                     else
                     {
-                        return "Year is out of date";
+                        return "2020";
                     }
+
                 }
                 set
                 {
@@ -67,11 +75,67 @@ namespace MotorVehicle1
                         return "Off";
                 }
             }
+
+            public enum TransType
+            {
+                Automatic,
+                Manual,
+                DualClutch,
+            }
+
+            private TransType _transmission;
+
+            public string Transmission
+            {
+                get
+                {
+                    if (_transmission == TransType.Automatic)
+                    {
+                        return "Automatic";
+                    }
+                    if (_transmission == TransType.DualClutch)
+                    {
+                        return "Hybrid";
+                    }
+                    if (_transmission == TransType.Manual)
+                    {
+                        return "Stick";
+                    }
+                    else
+                    {
+                    return "Automatic";
+                    }
+                }
+                set
+                {
+                    if (value != "Default")
+                    {
+                        if (value == "Automatic")
+                        {
+                            _transmission = TransType.Automatic;
+                        }
+                        if(value == "Hybrid")
+                        {
+                            _transmission = TransType.DualClutch;
+                        }
+                        if (value == "Stick")
+                        {
+                            _transmission = TransType.Manual;
+                        }
+                    }
+                    else
+                    {
+                        _transmission = TransType.Automatic;
+                    }
+                }
+            }
+
             public MotorVehicle()
             {
                 _model = "";
                 _make = "";
                 _year = "";
+                _transmission = TransType.Automatic;
                 _engineOn = false;
             }
 
@@ -99,7 +163,7 @@ namespace MotorVehicle1
 
         static void Main(string[] args)
         {
-            const int NUM_VEHICLES = 4;
+            const int NUM_VEHICLES = 5;
             MotorVehicle[] auto = new MotorVehicle[NUM_VEHICLES];
 
             for (int i = 0; i < NUM_VEHICLES; i++)
@@ -110,27 +174,37 @@ namespace MotorVehicle1
             auto[0].Make = "Toyota";
             auto[0].Model = "Camry";
             auto[0].Year = "2012";
+            auto[0].Transmission = "Default";
 
             auto[1].Make = "Honda";
             auto[1].Model = "Civic";
             auto[1].Year = "2015";
+            auto[1].Transmission = "Default";
 
             auto[2].Make = "Mazda";
             auto[2].Model = "3";
             auto[2].Year = "2017";
+            auto[2].Transmission = "Stick";
 
             auto[3].Make = "Subaru";
             auto[3].Model = "Legacy";
             auto[3].Year = "1967";
+            auto[3].Transmission = "Hybrid";
+
+            auto[4].Make = "Ford";
+            auto[4].Model = "Mustang";
+            auto[4].Year = "Default";
+            auto[4].Transmission = "Stick";
 
             for (int i = 0; i < NUM_VEHICLES; i++)
             {
-                Console.WriteLine("{0} {1} {2} {3}", auto[i].Year, auto[i].Make, auto[i].Model, auto[i].Engine);
+                Console.WriteLine("{0} {1} {2} {3} {4}", auto[i].Year, auto[i].Make, auto[i].Model, auto[i].Engine, auto[i].Transmission);
             }
             auto[0].Ignition("On", "1234");
             auto[1].Ignition("On", "0000");
             auto[2].Ignition("On", "4321");
             auto[3].Ignition("On", "0000");
+            auto[4].Ignition("On", "1234");
 
             Console.WriteLine("--------------");
             for (int i = 0; i < NUM_VEHICLES; i++)
@@ -142,6 +216,7 @@ namespace MotorVehicle1
             auto[1].Ignition("Off", "0000");
             auto[2].Ignition("Off", "4321");
             auto[3].Ignition("Off", "0000");
+            auto[4].Ignition("Off", "1234");
 
             Console.WriteLine("--------------");
             for (int i = 0; i < NUM_VEHICLES; i++)
