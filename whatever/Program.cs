52219ce810a6a032c7c6f015b6e9918d77c14edf
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2021-09-17 11:58:57 -0400

    Day 4 Bonus #1

--- whatever/Program.cs
+++ whatever/Program.cs
@@ -76,6 +76,28 @@ namespace MotorVehicle1
                 }
             }
 
+            private int _people;
+            public int People
+            {
+                get
+                {
+                    if (_people > 0 && _people <= 6)
+                    {
+                        return _people;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                set
+                {
+                    if (value != 0)
+                    {
+                        _people = value;
+                    }
+                }
+            }
             public enum TransType
             {
                 Automatic,
@@ -237,7 +259,7 @@ namespace MotorVehicle1
                 }
             }
 
-            public string _cab;
+            private string _cab;
 
             public string Cab
             {
@@ -282,13 +304,37 @@ namespace MotorVehicle1
 
             }
 
+
+            private int _people;
+
+            new private int People
+            {
+                get
+                {
+                    if (_people >= 2 && _people <= 6)
+                    {
+                        return _people;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                set
+                {
+                    if (value != 0)
+                    {
+                        _people = value;
+                    }
+                }
+            }
         }
 
 
 
-        public class Car:MotorVehicle
+        public class Car : MotorVehicle
         {
-            public string _type;
+            private string _type;
 
             public string Type
             {
@@ -334,7 +380,102 @@ namespace MotorVehicle1
 
             }
 
+            private int _people;
 
+            new private int People
+            {
+                get
+                {
+                    if (_people >= 2 && _people <= 6)
+                    {
+                        return _people;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                set
+                {
+                    if (value != 0)
+                    {
+                        _people = value;
+                    }
+                }
+            }
+        }
+
+        public class MotorCycle : MotorVehicle
+        {
+            public int _wheels;
+
+            public int Wheels
+            {
+                get
+                {
+                    if (_wheels == 2)
+                    {
+                        return _wheels;
+                    }
+                    if (_wheels == 3)
+                    {
+                        return _wheels;
+                    }
+                    if (_wheels == 4)
+                    {
+                        return _wheels;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                set
+                {
+                    if (value == 2)
+                    {
+                        _wheels = 2;
+                    }
+                    else if (value == 3)
+                    {
+                        _wheels = 3;
+                    }
+                    else if (value == 4)
+                    {
+                        _wheels = 4;
+                    }
+                    else
+                    {
+                        _wheels = 0;
+                    }
+                }
+
+
+            }
+
+            private int _people;
+
+            new private int People
+            {
+                get
+                {
+                    if (_people == 1 || _people == 2)
+                    {
+                        return _people;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                set
+                {
+                    if (value != 0)
+                    {
+                        _people = value;
+                    }
+                }
+            }
         }
 
 
@@ -353,34 +494,39 @@ namespace MotorVehicle1
             car[0].Year = "2012";
             car[0].Transmission = "Default";
             car[0].Type = "Sedan";
+            car[0].People = 4;
 
             car[1].Make = "Honda";
             car[1].Model = "Civic";
             car[1].Year = "2015";
             car[1].Transmission = "Default";
             car[1].Type = "Coupe";
+            car[1].People = 3;
 
             car[2].Make = "Mazda";
             car[2].Model = "3";
             car[2].Year = "2017";
             car[2].Transmission = "Stick";
             car[2].Type = "Hatchback";
+            car[2].People = 5;
 
             car[3].Make = "Subaru";
             car[3].Model = "Legacy";
             car[3].Year = "1967";
             car[3].Transmission = "Hybrid";
             car[3].Type = "Sedan";
+            car[3].People = 2;
 
             car[4].Make = "Ford";
             car[4].Model = "Mustang";
             car[4].Year = "Default";
             car[4].Transmission = "Stick";
             car[4].Type = "Coupe";
+            car[4].People = 6;
 
             for (int i = 0; i < NUM_VEHICLES; i++)
             {
-                Console.WriteLine("{0} {1} {2} {3} {4} {5}", car[i].Year, car[i].Make, car[i].Model, car[i].Type, car[i].Engine, car[i].Transmission);
+                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", car[i].Year, car[i].Make, car[i].Model, car[i].Type, car[i].Engine, car[i].Transmission, car[i].People);
             }
             car[0].Ignition("On", "1234");
             car[1].Ignition("On", "0000");
@@ -400,40 +546,98 @@ namespace MotorVehicle1
             truck[0].Year = "2020";
             truck[0].Transmission = "Default";
             truck[0].Cab = "Crew";
+            truck[0].People = 2;
 
             truck[1].Make = "Dodge";
             truck[1].Model = "3500";
             truck[1].Year = "2010";
             truck[1].Transmission = "Default";
             truck[1].Cab = "Extended";
+            truck[1].People = 4;
 
             truck[2].Make = "Chevrolet";
             truck[2].Model = "1500";
             truck[2].Year = "1998";
             truck[2].Transmission = "Default";
             truck[2].Cab = "Regular";
+            truck[2].People = 4;
 
             truck[3].Make = "GMC";
             truck[3].Model = "1500";
             truck[3].Year = "2020";
             truck[3].Transmission = "Default";
             truck[3].Cab = "Crew";
+            truck[3].People = 3;
 
             truck[4].Make = "Ford";
             truck[4].Model = "F150";
             truck[4].Year = "2017";
             truck[4].Transmission = "Hybrid";
             truck[4].Cab = "Extended";
+            truck[4].People = 6;
 
             for (int i = 0; i < NUM_VEHICLES; i++)
             {
-                Console.WriteLine("{0} {1} {2} {3} {4} {5}", truck[i].Year, truck[i].Make, truck[i].Model, truck[i].Cab, truck[i].Engine, truck[i].Transmission);
+                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", truck[i].Year, truck[i].Make, truck[i].Model, truck[i].Cab, truck[i].Engine, truck[i].Transmission, truck[i].People);
             }
             truck[0].Ignition("On", "1234");
             truck[1].Ignition("On", "0000");
             truck[2].Ignition("On", "4321");
             truck[3].Ignition("On", "0000");
             truck[4].Ignition(0, "On", "1234");
+
+
+            MotorCycle[] bike = new MotorCycle[NUM_VEHICLES];
+
+            for (int i = 0; i < NUM_VEHICLES; i++)
+            {
+                bike[i] = new MotorCycle();
+            }
+
+            bike[0].Make = "BMW";
+            bike[0].Model = "S1000 RP";
+            bike[0].Year = "2012";
+            bike[0].Transmission = "Default";
+            bike[0].Wheels= 2;
+            bike[0].People = 1;
+
+            bike[1].Make = "Ducati";
+            bike[1].Model = "Panigale";
+            bike[1].Year = "2015";
+            bike[1].Transmission = "Default";
+            bike[1].Wheels = 3;
+            bike[1].People = 2;
+
+            bike[2].Make = "Honda";
+            bike[2].Model = "Gold Wing";
+            bike[2].Year = "2017";
+            bike[2].Transmission = "Default";
+            bike[2].Wheels = 4;
+            bike[2].People = 2;
+
+            bike[3].Make = "Kawasaki";
+            bike[3].Model = "Ninja";
+            bike[3].Year = "1967";
+            bike[3].Transmission = "Default";
+            bike[3].Wheels = 3;
+            bike[3].People = 1;
+
+            bike[4].Make = "Suzuki";
+            bike[4].Model = "V Storm";
+            bike[4].Year = "Default";
+            bike[4].Transmission = "Default";
+            bike[4].Wheels = 2;
+            bike[4].People = 2;
+
+            for (int i = 0; i < NUM_VEHICLES; i++)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", bike[i].Year, bike[i].Make, bike[i].Model, bike[i].Wheels, bike[i].Engine, bike[i].Transmission, bike[i].People);
+            }
+            bike[0].Ignition("On", "1234");
+            bike[1].Ignition("On", "0000");
+            bike[2].Ignition(1, "On", "4321");
+            bike[3].Ignition("On", "0000");
+            bike[4].Ignition("On", "1234");
 
 
             Console.WriteLine("--------------");
@@ -459,6 +663,16 @@ namespace MotorVehicle1
             truck[3].Ignition("Off", "0000");
             truck[4].Ignition("Off", "1234");
 
+            for (int i = 0; i < NUM_VEHICLES; i++)
+            {
+                Console.WriteLine("{0} {1} {2}", bike[i].Make, bike[i].Model, bike[i].Engine);
+            }
+            bike[0].Ignition("On", "1234");
+            bike[1].Ignition("On", "0000");
+            bike[2].Ignition("On", "4321");
+            bike[3].Ignition("On", "0000");
+            bike[4].Ignition("On", "1234");
+
             Console.WriteLine("--------------");
             for (int i = 0; i < NUM_VEHICLES; i++)
             {
@@ -468,7 +682,10 @@ namespace MotorVehicle1
             {
                 Console.WriteLine("{0} {1} {2}", truck[i].Make, truck[i].Model, truck[i].Engine);
             }
-
+            for (int i = 0; i < NUM_VEHICLES; i++)
+            {
+                Console.WriteLine("{0} {1} {2}", bike[i].Make, bike[i].Model, bike[i].Engine);
+            }
 
             Console.ReadLine();
         }
